e3840835d16969f7db48bf16763924acfbc191e9
Cameron Malone <cameronmkm2@gmail.com>
2023-07-17 12:39:14 +1000

    add showcase to main app

--- Task4.1P/src/App.tsx
+++ Task4.1P/src/App.tsx
@@ -2,14 +2,24 @@
 import './App.css';
 import Navbar from '@/components/Navbar';
 import Hero from '@/components/Hero';
-import ProfileCard from '@/components/ProfileCard';
+// import ProfileCard from '@/components/ProfileCard';
+import Showcase from '@/components/Showcase';
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
       <Hero></Hero>
-      <ProfileCard></ProfileCard>
+      <Showcase
+        type="developer"
+        title="Featured Freelancers"
+        numProfiles={2}
+      ></Showcase>
+      <Showcase
+        type="customer"
+        title="Featured Customers"
+        numProfiles={2}
+      ></Showcase>
     </>
   );
 }
