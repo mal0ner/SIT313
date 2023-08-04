37a69deba40bb272e82d8004ee4cd4d7f81da57e
Cameron Malone <cameronmkm2@gmail.com>
2023-08-04 11:07:39 +1000

    add login page to navbar links

--- Task7.1P/src/components/Navbar.tsx
+++ Task7.1P/src/components/Navbar.tsx
@@ -55,7 +55,7 @@ function Navbar() {
                   href="#"
                   className={navigationMenuTriggerStyle()}
                 >
-                  Login
+                  <Link to={'/login'}>Login</Link>
                 </NavigationMenuLink>
               </NavigationMenuItem>
 
