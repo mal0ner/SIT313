51612ea36167c58196c36196d8caa98bf075e124
Cameron Malone <cameronmkm2@gmail.com>
2023-08-04 11:33:49 +1000

    add basic login form elements

--- Task7.1P/src/components/Login.tsx
+++ Task7.1P/src/components/Login.tsx
@@ -1,8 +1,75 @@
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+
+const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
+
 export default function Login() {
+  const loginForm = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof loginSchema>) {
+    //Do something with form values
+    //this will use firebase to check user against existing user
+    //documents
+    console.log(values);
+  }
+
   return (
     <>
       <div className="m-10 flex flex-col items-center justify-center">
-        <h1>Hello Login Page</h1>
+        <Form {...loginForm}>
+          <form
+            onSubmit={loginForm.handleSubmit(onSubmit)}
+            className="flex flex-col gap-10 max-w-prose"
+          >
+            <FormField
+              control={loginForm.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input placeholder="Email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={loginForm.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input placeholder="Password" type="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button type="submit">Login</Button>
+          </form>
+        </Form>
       </div>
     </>
   );
