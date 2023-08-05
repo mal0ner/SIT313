3dd5b76d21de3ca28b4fc855b34cb5567695fb0e
Cameron Malone <cameronmkm2@gmail.com>
2023-08-05 12:25:33 +1000

    new Login component complete with form validation and firebase auth interface

--- Task7.1P/src/components/Login.tsx
+++ Task7.1P/src/components/Login.tsx
@@ -8,14 +8,29 @@ import {
   FormControl,
   FormField,
   FormItem,
+  FormLabel,
   FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import {
+  checkUserExists,
+  loginAuthUserWithEmailAndPassword,
+} from '@/utils/firebase';
 
-const loginSchema = z.object({
-  email: z.string().email(),
-  password: z.string(),
-});
+const loginSchema = z
+  .object({
+    email: z.string().email(),
+    password: z.string().min(1, { message: 'Password required to login' }),
+  })
+  .refine(
+    async (data) => {
+      return await checkUserExists(data.email);
+    },
+    {
+      path: ['email'],
+      message: 'We could not find an account under that email',
+    },
+  );
 
 export default function Login() {
   const loginForm = useForm<z.infer<typeof loginSchema>>({
@@ -26,28 +41,49 @@ export default function Login() {
     },
   });
 
-  function onSubmit(values: z.infer<typeof loginSchema>) {
+  //TODO: See https://stackoverflow.com/questions/75148276/email-validation-with-zod
+  //for information on how to correctly check emails against db and show
+  //zod schema error messages
+
+  async function onSubmit(values: z.infer<typeof loginSchema>) {
     //Do something with form values
     //this will use firebase to check user against existing user
     //documents
-    console.log(values);
+    try {
+      return await loginAuthUserWithEmailAndPassword(
+        values.email,
+        values.password,
+      );
+    } catch (error: any) {
+      if (error.code === 'auth/wrong-password') {
+        loginForm.setError('password', { message: 'Incorrect Password' });
+      } else {
+        alert(
+          error.message.replace('Firebase: ', '').replace(/\(auth.*\)\.?/, ''),
+        );
+      }
+    }
   }
 
   return (
     <>
-      <div className="m-10 flex flex-col items-center justify-center">
+      <div className="m-10 flex flex-col gap-10 items-center justify-center w-1/2">
+        <div className="text-center flex flex-col gap-2">
+          <h1 className="text-4xl font-yeseva">Welcome back!</h1>
+        </div>
         <Form {...loginForm}>
           <form
             onSubmit={loginForm.handleSubmit(onSubmit)}
-            className="flex flex-col gap-10 max-w-prose"
+            className="flex flex-col gap-5 w-full max-w-prose"
           >
             <FormField
               control={loginForm.control}
               name="email"
               render={({ field }) => (
                 <FormItem>
+                  <FormLabel>Name</FormLabel>
                   <FormControl>
-                    <Input placeholder="Email" {...field} />
+                    <Input placeholder="jschmiddy@aol.com" {...field} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -59,15 +95,22 @@ export default function Login() {
               name="password"
               render={({ field }) => (
                 <FormItem>
+                  <FormLabel>Password</FormLabel>
                   <FormControl>
-                    <Input placeholder="Password" type="password" {...field} />
+                    <Input
+                      placeholder="**********"
+                      type="password"
+                      {...field}
+                    />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
               )}
             />
 
-            <Button type="submit">Login</Button>
+            <Button className="w-1/2 place-self-center" type="submit">
+              Login
+            </Button>
           </form>
         </Form>
       </div>
