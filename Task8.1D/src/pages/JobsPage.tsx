2a2afc3895a77b40e832c207947f8ce4ca3dfbce
Cameron Malone <cameronmkm2@gmail.com>
2023-09-12 01:02:25 +1000

    add basic intro and search filter layout

--- Task8.1D/src/pages/JobsPage.tsx
+++ Task8.1D/src/pages/JobsPage.tsx
@@ -2,9 +2,22 @@ import { Post, getPosts } from '@/utils/firebase';
 import { useEffect, useState } from 'react';
 import PostCard from '@/components/PostCard';
 
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { Separator } from '@/components/ui/separator';
+
 function JobsPage() {
   const [data, setData] = useState<Post[] | []>([]);
-  // const [query, setQuery] = useState('');
+  const [filter, setFilter] = useState<string>('');
+  const [query, setQuery] = useState<string[]>([]);
+
+  function handleFilterChange() {
+    const searchItems = filter.split(',');
+    searchItems.forEach((s, idx) => {
+      searchItems[idx] = s.trim().toLowerCase();
+    });
+    setQuery(searchItems);
+  }
 
   useEffect(() => {
     async function getData() {
@@ -14,12 +27,36 @@ function JobsPage() {
     getData();
   }, []);
   return (
-    <div className="grid place-items-center min-h-screen">
-      <div className="flex flex-col gap-5 items-center">
-        <h1 className="text-2xl md:text-4xl font-yeseva">Jobs Page</h1>
-        {data.map((post: Post, index) => (
-          <PostCard key={index} {...post} />
-        ))}
+    <div className="">
+      <div className="flex flex-col gap-5 w-full p-6 md:p-12 items-center justify-center text-justify">
+        <h1 className="text-2xl md:text-4xl font-yeseva">Find Jobs</h1>
+        <p className="font-josefin max-w-prose text-lg">
+          Welcome to the DevLink Marketplace Job's Page! This is the heart of
+          our website, and the place where passionate employers meet the next
+          member of their team.
+        </p>
+        <p className="font-josefin max-w-prose text-lg">
+          Don't forget to use the custom filter and search options below to find
+          the best jobs for you!
+        </p>
+      </div>
+      <Separator />
+      <div className="flex flex-col gap-3 p-6 md:p-12">
+        <p className="font-bold">Search</p>
+        <Input
+          type="search"
+          placeholder="Angular, Javascript, Typescript"
+          onChange={(e) => setFilter(e.target.value)}
+        />
+        <Button onClick={handleFilterChange}>Apply</Button>
+        <p>{query}</p>
+      </div>
+      <div className="w-full flex items-center mt-12 mb-12">
+        <div className="flex flex-col gap-5 items-center w-full">
+          {data.map((post: Post, index) => (
+            <PostCard key={index} {...post} />
+          ))}
+        </div>
       </div>
     </div>
   );
