24543c351691b65e55a78c841da591fbcd5c1f04
Cameron Malone <cameronmkm2@gmail.com>
2023-09-25 13:39:50 +1000

    add analytics capabilities

--- Task6.1HD/src/utils/firebase.ts
+++ Task6.1HD/src/utils/firebase.ts
@@ -2,7 +2,7 @@
 // import 'dotenv/config';
 
 import { initializeApp } from 'firebase/app';
-// import { getAnalytics } from 'firebase/analytics';
+import { getAnalytics } from 'firebase/analytics';
 import {
   getAuth,
   createUserWithEmailAndPassword,
@@ -53,7 +53,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
-// const analytics = getAnalytics(app);
+const analytics = getAnalytics(app);
 
 export type UserDoc = {
   createdAt: Timestamp;
