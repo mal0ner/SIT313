76bff8430178841e9bcd75ba2404c66dc5889ac8
Cameron Malone <cameronmkm2@gmail.com>
2023-10-01 13:49:58 +1100

    add app title

--- Task6.1HD/src/App.tsx
+++ Task6.1HD/src/App.tsx
@@ -2,8 +2,12 @@ import './App.css';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    document.title = 'DevLink Marketplace';
+  }, []);
   return (
     <>
       <Navbar></Navbar>
